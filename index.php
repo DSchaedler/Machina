e903cf1cb56c4de81206c7b88317b7849d0fb48f
Beorn619 <qioime@yahoo.com>
2018-04-23 22:44:09 -0400

    The site is now completely mobile optimized with testing that I have done. Div links have been removed since they were causing problems, underlines of links have been removed. (might be added back?) changed link color to Discord Blue but darker. Changed link color near discord icon to show that it is a link. Reduced size of world map.

--- index.php
+++ index.php
@@ -20,27 +20,18 @@
 	<body>
 		<h1>Machina Minecraft Server</h1>
 		<h2 onclick="SelectText('ip')" id="ip" title="Click to copy!">mc.machina.fun</h2>
-
 		<div id="discord-wrapper">
 			<a class="div_link" target="_blank" href="<?php echo $invite; ?>"><img src="images/Discord-Logo-Color.svg" id="discord-logo" alt="The Discord Logo. A video game controller with eyes."></a>
 			<a class="div_link" target="_blank" href="<?php echo $invite; ?>"><p id="discord-json"><strong><?php echo $online; ?></strong> Members Online</p></a>
 		</div>
-		
 		<div id="info-wrapper">
-			<a class="div_link" href="https://www.atlauncher.com/pack/BevosTechPack" target="_blank">
 				<img src="images/bevo.png" id="bevo-logo" alt="Bevo's Tech Pack." class="auto-style1">
-				<p id="bevo-text">This server runs on the latest version of <u>Bevo's Tech Pack 1.7.10</u></p>
-			</a>
-
-			<a target="_blank" href="https://www.atlauncher.com/downloads" class="div_link">
+				<p id="bevo-text">This server runs on the latest version of <a class="div_link" href="https://www.atlauncher.com/pack/BevosTechPack" target="_blank">Bevo's Tech Pack 1.7.10</a></p>
 				<img src="images/at.png" id="at-logo" alt="AT Launcher">
-				<p id="bevo-text">You will need <u>AT Launcher</u> to run this pack.</p>
-			</a>
+				<p id="bevo-text">You will need <a target="_blank" href="https://www.atlauncher.com/downloads" class="div_link">AT Launcher</a> to run this pack.</p>
 		</div>
-
 		<h2>World Map</h2>
 		<iframe src="http://machina.mine.bz:8120/"></iframe>
-
 		<script type="text/javascript" src="scripts/main.js"></script>
 	</body>
 </html>
